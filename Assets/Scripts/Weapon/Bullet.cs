4cd7658228ca4c0d34e9cf74a18cf18e1ff9dc1b
unknown <spd2020.work@gmail.com>
2023-03-27 19:54:10 +0500

    testing

--- Assets/Scripts/Weapon/Bullet.cs
+++ Assets/Scripts/Weapon/Bullet.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
-    public float bulletLife = 3f;
+    public float bulletLife = 3f;// время жизни пули
 
     private void Awake()
     {
