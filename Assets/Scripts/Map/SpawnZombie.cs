f71c2e48e00635406ae856158bad116e13c11dcd
unknown <spd2020.work@gmail.com>
2023-04-23 15:04:57 +0500

    Rewrite script spawner Zombie

--- Assets/Scripts/Map/SpawnZombie.cs
+++ Assets/Scripts/Map/SpawnZombie.cs
@@ -6,16 +6,20 @@ public class SpawnZombie : MonoBehaviour
 {
     public GameObject zombie; // Объект Зомби
     public GameObject[] zSpawners; // Спавнера
+    [SerializeField] public Vector3[] spawnPosition;
 
     public int count = 5; // Кол-во зомби будет спавниться
     public int indexSpawwners = 0; // Индекс спавнера
 
     public bool waveIsDone = false; // Волна закончилась
 
-    private void SpawnZ(GameObject spawner)
+    private void SpawnZ(GameObject spawner,int indexPosiiton)
     {
-        Vector3 spawn = new Vector3(spawner.transform.position.x + Random.Range(0, 2), spawner.transform.position.y + Random.Range(0, 2), spawner.transform.position.z);
-        Instantiate(zombie, spawner.transform.position, Quaternion.identity); // спавним зомби
+
+        Vector3 spawn = new Vector3(spawner.transform.position.x + spawnPosition[indexPosiiton].x, 
+                                    spawner.transform.position.y, 
+                                    spawner.transform.position.z + spawnPosition[indexPosiiton].z);
+        Instantiate(zombie, spawn, Quaternion.identity); // спавним зомби
     }
 
     private void Update()
@@ -24,7 +28,8 @@ public class SpawnZombie : MonoBehaviour
         {
             for (int i = 0; i < count; i++)
             {
-                SpawnZ(zSpawners[indexSpawwners]);
+                int rnd = Random.Range(0, spawnPosition.Length);
+                SpawnZ(zSpawners[indexSpawwners], rnd);
             }
             waveIsDone = false;
         }
