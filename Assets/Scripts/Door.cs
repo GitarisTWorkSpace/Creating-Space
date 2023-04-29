c7c6c8bc933b02a058dc798006633d26dc8525a6
Vlad <k1ri4enko03@yandex.ru>
2023-04-29 14:31:20 +0500

    Vl

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour
+{
+	private bool _isOpened;
+	[SerializeField] private Animator _animator;
+
+
+	public void Open()
+	{
+		_animator.SetBool("isOpening", _isOpened);
+		_isOpened = !_isOpened;
+	}
+}
